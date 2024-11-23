d825ad27b83f975f01981124baf162cc4f4fd1d1
mefe-b <meakkocaoglu@gmail.com>
2024-11-23 15:58:12 +0300

    docs: variable usage explained

---
+++ Learn_ruby/2-degiskenler.rb
@@ -0,0 +1,12 @@
+x = "a" # string - metin
+y = 2 # integer - sayı
+
+ad = "levent" # string
+soyad = "özbilgiç" #string
+ad_soyad = ad + soyad #string
+
+a = 1.1 #float
+b = 2.36 #double
+x, y = 1, 2
+x,y = y,x # x'e ve y'ye atanan değerleri değiştirir
+x , *y = 1 2 3 4 # burada * sembolü değişkene birden fazla değişken atanabileceği anlamına gelir
