40d2c867feab906abc82c6394c52a24373c0a652
mefe-b <meakkocaoglu@gmail.com>
2024-11-24 15:12:15 +0300

    docs: explained the difference between puts and print

---
+++ Learn_ruby/9-puts_ve_print.rb
@@ -0,0 +1,15 @@
+#puts "1. çıktı: bu değer konsola yazdırılacak"
+#puts "2. çıktı: bu değer konsola yazdırılacak"
+
+#print "3. çıktı: bu değer konsola yazdırılacak"
+#print "4. çıktı: bu değer konsola yazdırılacak"
+
+puts "1. çıktı:", "bu değer", "konsola", "yazdırılacak"
+print "2. çıktı:", "bu değer", "konsola", "yazdırılacak"
+
+arr = [1,2,3,4,5]
+
+puts arr
+print arr
+
+# Print 
