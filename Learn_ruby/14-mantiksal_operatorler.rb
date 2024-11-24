76ebdf4b358b952f84c488d28b471fb4ad89cf8c
mefe-b <meakkocaoglu@gmail.com>
2024-11-24 20:37:55 +0300

    docs: explained logical operators

---
+++ Learn_ruby/14-mantiksal_operatorler.rb
@@ -0,0 +1,30 @@
+a = false
+b = false
+
+puts a and b
+
+# or : veya operatörü
+a = true
+b = false
+
+puts a or b
+
+# && : and operatörü
+a = false
+b = true
+
+puts a && b
+
+# || : veya operatörü
+a = false
+b = true
+
+puts a || b
+
+# ! : tersi operatörü
+a = false
+puts !a
+
+# not : tersi operatörü
+a = false
+puts (not a)
