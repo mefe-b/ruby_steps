f9a00e28bb99ca15bdbdec70509ffac71ca2d2ee
mefe-b <meakkocaoglu@gmail.com>
2024-11-24 18:11:53 +0300

    docs: explained assignment operators

---
+++ Learn_ruby/12-atama_operatorleri.rb
@@ -0,0 +1,30 @@
+# = : atama operatörü
+# sağındaki değeri solundaki değişkene atar
+x = 4 #x değişkenine 1 değerini atadık
+
+# += : kendi değerini sağındaki değer ile toplar ve tekrar atama yapar
+puts x += 2
+
+# -= : artı eşittir operatörünün tersi
+puts x -= 2
+
+# *= : kendi değerini sağındaki değer ile çarpar ve tekrar atama yapar
+puts x *= 2
+
+# /= : kendi değerini sağındaki değere böler ve tekrar atama yapar
+puts x /= 2
+
+# %= : kendi değerini sağındaki değer ile modunu alıp tekrar atar
+puts x %= 2
+
+# **= : kendi değerini sağındaki değere göre üssünü alır ve tekrar atama yapar
+x = 2
+puts x **= 2
+
+a = 1
+b = 2
+c = 3
+
+a,b,c = 1,2,3
+
+puts a,b,c
