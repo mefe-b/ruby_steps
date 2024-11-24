c8bcd65ff02f2731435597ade624193d7d7f6c51
mefe-b <meakkocaoglu@gmail.com>
2024-11-24 12:59:57 +0300

    docs: symbols and constants introduced

---
+++ Learn_ruby/7-semboller_ve_sabitler.rb
@@ -0,0 +1,21 @@
+isim = "Berkant"
+#puts isim.object_id
+
+isim = "Berkant"
+#puts isim.object_id
+
+isim = :berkant 
+#puts isim.object_id
+
+isim = :berkant
+#puts isim.object_id
+
+PI = 3,14
+puts "#{PI}"
+
+PI = 3.1415
+puts "#{PI}"
+
+# Semboller bilgisayar belleğinde bir kere oluşturulur ve program süresince oluşturulur. ":" ile tanımlanır.
+# Sabitler büyük harfle başlamalıdır, içine programda kullanacağınız sabit bir değer atayabilirsiniz.
+# Sabitleri oluştururken tamamını büyük yazmak daha iyidir diğer programlardan farklı olarak değiştirilebilirler.
