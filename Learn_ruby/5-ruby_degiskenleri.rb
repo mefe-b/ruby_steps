edea8ecbd10cde797c5996581e520ccaab9c5a0e
mefe-b <meakkocaoglu@gmail.com>
2024-11-24 11:23:51 +0300

    docs: class variables explained

---
+++ Learn_ruby/5-ruby_degiskenleri.rb
@@ -0,0 +1,31 @@
+class Personel
+  @@personel_sayisi = 0
+  
+  def personel_bilgileri(ad, soyad, meslek)
+    @personel_adi = ad
+    @personel_soyadi = soyad
+    @personel_meslegi = meslek
+    @@personel_sayisi += 1
+  end
+  
+  def toplam_personel
+    puts "Toplam personel: #{@@personel_sayisi}"
+  end
+end
+
+p1 = Personel.new
+p1.personel_bilgileri("Levent", "Özbilgiç", "Yazılımcı")
+
+p2 = Personel.new
+p2.personel_bilgileri("Enes", "Çiğdem", "Öğrenci")
+
+class Ogrenci < Personel
+  def isim_yazdir
+    puts "Personelin adı ve soyadı: #{@personel_adi} #{@personel_soyadi}"
+  end
+end
+
+o1 = Ogrenci.new
+o1.personel_bilgileri("Enes", "Çiğdem", "Öğrenci")
+o1.isim_yazdir
+o1.toplam_personel
