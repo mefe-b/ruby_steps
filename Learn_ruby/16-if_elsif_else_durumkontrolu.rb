98ccdde8197ba546fa272db36f7084ac7d531e76
mefe-b <meakkocaoglu@gmail.com>
2024-11-25 22:51:20 +0300

    docs: explained if-elsif-else conditional statements

---
+++ Learn_ruby/16-if_elsif_else_durumkontrolu.rb
@@ -0,0 +1,33 @@
+a = 2
+b = 6
+
+if a+b == 8
+  puts "a+b nin değeri: 8 dir."
+end
+
+if a > 5
+  puts "a nın değeri 5 den büyüktür"
+else
+  puts "a nın değeri 5 den küçüktür"
+end
+
+x = 9
+y = 9
+
+if x < y
+  puts "x değeri y den küçüktür"
+elsif x > y
+  puts "x değeri y den büyüktür"
+else
+  puts "x ve ye değeri birbirine eşittir"
+end
+
+urun = "elma"
+
+if urun == "çilek"
+  puts "gelen ürün: çilek"
+elsif urun == "kiraz"
+  puts "gelen ürün kiraz"
+elsif urun == "elma"
+  puts "gelen ürün: elma"  
+end
