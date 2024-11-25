142bbbd43030f40180399fa5d294ddd1e1c0de2b
mefe-b <meakkocaoglu@gmail.com>
2024-11-25 23:14:51 +0300

    docs: explained unless-else conditional statements

---
+++ Learn_ruby/17-unless_else_durum_kontorlu.rb
@@ -0,0 +1,23 @@
+a = 1
+b = 3
+
+unless a < 0
+  puts "a nın değeri #{a}"
+end
+
+toplam = 9
+
+unless toplam < 10
+  puts "toplam değişkeninin değeri 10 dan küçük değildir"
+else
+  puts "toplam değeri 10 dan küçüktür."
+end
+
+x = 4
+y = 6
+
+x > y unless puts "x değeri y den büyük değildir."
+
+sayi = 9
+
+sayi%2 == 0 unless puts "sayi değişkeninin değeri tek sayıdır"
