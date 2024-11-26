83d7fd67e5779f3e17d7f87ea91f028d9780a836
mefe-b <meakkocaoglu@gmail.com>
2024-11-26 14:20:27 +0300

    docs: explained heredoc usage in Ruby

---
+++ Learn_ruby/28-heredoc_kullanimi.rb
@@ -0,0 +1,10 @@
+kalan_gun = 10
+
+h = <<-ACIKLAMA 
+          Aboneliğiniz yakında bitiyor!
+            Ücretsiz denemeniz #{kalan_gun} dün içinde sona erecek.
+        Lütfen fatura bilgilerinizi giriniz!
+ACIKLAMA
+
+print h
+#  <<- operatörü girinti farklarını göz ardı eder.
