9655eb3e622ec1fdec48350db799b06db9d04e26
mefe-b <meakkocaoglu@gmail.com>
2024-11-26 11:42:02 +0300

    docs: explained text creation in Ruby

---
+++ Learn_ruby/26-metin_olusturma.rb
@@ -0,0 +1,31 @@
+a = 2
+
+puts 'metin #{a}'
+puts "metin #{a}"
+
+puts "metin 'metin'"
+puts 'metin "metin"'
+
+puts "merhaba".size()
+
+s = String.new("metin")
+puts s
+
+s1 = "metin 1-"
+s2 = "metin 2"
+s3 = s1 + s2
+
+puts s3
+
+puts "metin 1" + "metin 2"
+puts "metin 1"+ " " + "metin 2"
+
+m = "metin"
+puts m[1]
+
+puts m[0..2]s
+
+=begin 
+Tek tırnak ('): Sadece harfleri ve sayıları içerir, değişkenleri ya da özel karakterleri işlemez.
+Çift tırnak ("): Değişkenleri içerik içinde işlemeye olanak tanır ve özel karakterlere duyarlıdır.
+=end
