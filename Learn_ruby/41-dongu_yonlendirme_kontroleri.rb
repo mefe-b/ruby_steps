72c2d823ce835a3a03c0c2fba9d8542e0e553af0
mefe-b <meakkocaoglu@gmail.com>
2024-11-26 23:41:21 +0300

    feat: add explanations for break, next, and redo in Ruby control flow

---
+++ Learn_ruby/41-dongu_yonlendirme_kontroleri.rb
@@ -0,0 +1,24 @@
+#break komutu Döngüleri erken sonlandırmak için kullanılır.
+i = 0
+
+while i < 10
+  puts i
+  i += 1
+  break if i == 5
+end
+
+#next komutu Döngüdeki bir iterasyonu atlayıp bir sonraki iterasyona geçmek için kullanılır.
+5.times do |i|
+  next if i == 3
+  puts i
+end
+
+#redo komutu 
+(0..5).each do |i|
+  if i > 4 && i < 10
+    puts "değer: #{i}"
+    i += 1
+    redo
+  end
+  puts "değer: #{i}"
+end
