6f3a53521f0d412491285d3f2c7cbb785cbe155f
mefe-b <meakkocaoglu@gmail.com>
2024-11-28 23:12:28 +0300

    docs: array sorting explained

---
+++ Learn_ruby/53-dizilerde_siralama.rb
@@ -0,0 +1,8 @@
+sayilar = [6,4,5,2,1,3]
+harfler = ["b","c", "e", "a", "d"]
+  
+puts sayilar.sort!().inspect
+puts harfler.sort!().inspect
+
+puts sayilar.sort!().reverse.inspect
+puts harfler.sort!().reverse.inspect
