891ed5aab779272b3e3ab3d81dc2c57585f7fe27
mefe-b <meakkocaoglu@gmail.com>
2024-11-29 11:44:25 +0300

    feat: defining classes and methods within a module explained

---
+++ Learn_ruby/63-modul_icinde_sinif_ve_metot_tanimlama.rb
@@ -0,0 +1,21 @@
+module Yazici
+  VERSIYON = "1.2"
+  
+  class Fax
+    def fax_gonder(metin)
+      puts "Fax gönderildi. #{metin}"
+    end
+  end
+  
+  def self.versiyon
+    puts "Versiyon #{VERSIYON}"
+  end
+  
+  def guncelle
+    puts "Yazıcı versiyonu yükseltiliyor..."
+  end
+end
+
+yeni_fax = Yazici::Fax.new
+yeni_fax.fax_gonder("Merhaba")
+puts Yazici::versiyon()
